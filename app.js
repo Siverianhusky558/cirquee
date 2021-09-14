8636fdf986bd190565cc156483783c95cff12398
AbdulRahman Al Sharqawy <alaskasibera234@gmail.com>
2021-09-14 21:29:38 +0300

    Trying Deploying Again

--- app.js
+++ app.js
@@ -161,6 +161,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-  console.log('NEW CAMPGROUNDS!!!');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on PORT: ${port}`);
 });
