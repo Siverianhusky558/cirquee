1c433ec759f81cb894e46f082272ed4741f5be4b
AbdulRahman <alaskasiberia234@gmail.com>
2022-06-28 16:10:15 +0300

    Added bottom navbar

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,24 @@
-const editInputs = document.querySelectorAll('input');
+var nav = document.getElementById("comp");
+var mob = document.getElementById("mob");
+var mq = window.matchMedia("(max-width: 640px)");
 
-for (let input of editInputs) {
-  if (parseInt(input.value) === rating) {
-    input.checked = true;
+if (mq.matches) {
+  nav.classList.add("d-none");
+  mob.classList.remove("d-none");
+} else {
+  nav.classList.remove("d-none");
+  mob.classList.add("d-none");
+}
+
+mq.addEventListener("change", checkIfMatch);
+
+function checkIfMatch(e) {
+  if (e.matches) {
+    nav.classList.add("d-none");
+    mob.classList.remove("d-none");
+  } else {
+    nav.classList.remove("d-none");
+    mob.classList.add("d-none");
   }
 }
 
---
+++ public/javascripts/stars.js
@@ -0,0 +1,7 @@
+let editInputs = document.querySelectorAll("input");
+
+for (let input of editInputs) {
+  if (parseInt(input.value) === rating) {
+    input.checked = true;
+  }
+}
